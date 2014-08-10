db14236747ae81ab5abef116ea077bb97c5d1dfe
zzdever <zzdever@gmail.com>
2014-08-10 10:18:30 +0000

    zzdever 20140715 add mmi2c

---
+++ eeprom/config.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+insmod /lib/modules/3.8.7-yocto-standard/kernel/drivers\
+/i2c/i2c-dev.ko
+
+echo 29 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio29/direction
+echo 0 > /sys/class/gpio/gpio29/value
